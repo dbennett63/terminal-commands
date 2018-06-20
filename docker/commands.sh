b233b5925210415ac60bca392f25328764fe2f66
Dustin Bennett <bennett.dustin@gmail.com>
2018-06-20 14:31:38 -0400

    add zsh support

--- docker/commands.sh
+++ docker/commands.sh
@@ -6,3 +6,16 @@ docker run -p 3000:3000 helloworld
 
 ## Needed to connect to dockerd
 eval "$(docker-machine env default)"
+
+## Push helloworld app
+docker build -t gcr.io/extreme-signer-191920/helloworld .
+gcloud docker -- push gcr.io/extreme-signer-191920/helloworld
+
+#create container cluster
+gcloud container clusters create hello-cluster --num-nodes=3
+
+gcr.io/extreme-signer-191920/helloworld
+
+kubectl run helloworld --image=gcr.io/extreme-signer-191920/helloworld --port 3000
+
+kubectl expose deployment hello-web --type=LoadBalancer --port 80 --target-port 8080
---
+++ shell/compress.sh
@@ -0,0 +1,11 @@
+# tar.gz
+gunzip -c foo.tar.gz | tar xopf -
+
+# tar.gz with file listing
+gunzip -c foo.tar.gz | tar xopft -
+
+#tarball
+tar xopf -
+
+# zip
+unzip -a foo.zip
